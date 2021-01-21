065d6e35e063ceee673b593a2c0e126890093633
masemoel <az.az.az.mjme@gmail.com>
2021-01-21 09:13:58 +0100

    picasso: Add ODM and system_EXT to the dynamic script
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- recovery/root/init.recovery.qcom.sh
+++ recovery/root/init.recovery.qcom.sh
@@ -9,10 +9,14 @@ setenforce 0
 sleep 5
 mount -w /product > /dev/null
 mount -w /vendor > /dev/null
+mount -w /odm > /dev/null
+mount -w /system_ext > /dev/null
 mount -w /system_root > /dev/null
 
 sleep 1
 umount /product > /dev/null
 umount /vendor > /dev/null
+umount /odm > /dev/null
+umount /system_ext > /dev/null
 umount /system_root > /dev/null
 #
