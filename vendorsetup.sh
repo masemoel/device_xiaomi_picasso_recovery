79feaf6483b2145ecc7030aa7b2d7bde52d7bcea
masemoel <az.az.az.mjme@gmail.com>
2021-02-10 18:41:24 +0100

    picasso: Move OF flags to vendorsetup.sh
    Seems that some of them doesn't work properly in BoardConfig, besides being mandatory to officially maintain this recovery.
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,85 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="picasso"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+    if [ -n "$chkdev" ]; then 
+        FOX_BUILD_DEVICE="$FDEVICE"
+    else
+        chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+        [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+    fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+    fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+    export FOX_USE_NANO_EDITOR=1
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+    export OF_NO_SPLASH_CHANGE=1
+    export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+    export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+    export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION=1
+    export OF_NO_RELOAD_AFTER_DECRYPTION=1
+    export OF_NO_MIUI_PATCH_WARNING=1
+    export FOX_DISABLE_APP_MANAGER=1
+    export OF_MAINTAINER=masemoel
+    export FOX_VERSION=1.3
+    export OF_SCREEN_H=2460
+    export OF_STATUS_H=95
+    export OF_ALLOW_DISABLE_NAVBAR=0
+    export OF_PATCH_AVB20=1
+    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+    export OF_CHECK_OVERWRITE_ATTEMPTS=1
+    export OF_FBE_METADATA_MOUNT_IGNORE=1
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+    export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+    export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
+    export OF_SILENCE_MIUI_MOUNT_COMPLAINTS=1
+    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    export FOX_ADVANCED_SECURITY=1
+    export OF_NO_SAMSUNG_SPECIAL=1
+    export FOX_INSTALLER_DEBUG_MODE=1
+    export FOX_R11=1
+    export OF_USE_TWRP_SAR_DETECT=1
+    export OF_TARGET_DEVICES=picasso,picasso_48m
+    export OF_DONT_KEEP_LOG_HISTORY=1
+    export OF_USE_GREEN_LED=0
+    export FOX_DELETE_AROMAFM=1
+    export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+    export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
+    export FOX_USE_BASH_SHELL=1
+    export FOX_ASH_IS_BASH=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_RESET_SETTINGS=0
+
+	# Let's see which are our build vars
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+        export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+        export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    fi
+fi
