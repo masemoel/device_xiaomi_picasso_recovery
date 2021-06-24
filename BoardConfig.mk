e03c7f63ca4f0508b63802fb83ed46818b7b99cc
Sushrut Gupta <guptasushrut@gmail.com>
2021-06-25 01:04:25 +0530

    picasso: Fix MIUI 12.x Android 11 decryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,11 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
+# MIUI 12.x Android 11 decryption fix
+BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += \
+    --prop com.android.build.boot.os_version:$(PLATFORM_VERSION) \
+    --prop com.android.build.boot.security_patch:$(PLATFORM_SECURITY_PATCH)
+
 # TWRP stuff
 TW_THEME := portrait_hdpi
 BOARD_HAS_NO_REAL_SDCARD := true
