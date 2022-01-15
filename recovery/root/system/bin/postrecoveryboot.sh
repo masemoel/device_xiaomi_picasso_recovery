ac9c4b8f0bd20b1eba6b499b0f6da09a6f5edb9d
Darth9 <reyprimerjefe@gmail.com>
2022-01-15 15:50:37 +0000

    picasso: Update the postrecoveryboot script
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -1,25 +1,40 @@
 #!/sbin/sh
 #
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2022 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
 # TODO: this kludge is needed to prevent issues with mounting
 # system and vendor in some zip installers and in the gui
 #
-sleep 1
-mount -w /product > /dev/null 2>&1
-mount -w /vendor > /dev/null 2>&1
-mount -w /odm > /dev/null 2>&1
-mount -w /system_ext > /dev/null 2>&1
-mount -w /system_root > /dev/null 2>&1
+set_read_write_partitions() {
+local Parts=("system" "vendor" "product" "odm" "system_ext");
+  for i in "${Parts[@]}"
+  do
+     echo "I:OrangeFox: setting $i to read/write" >> /tmp/recovery.log;
+     blockdev --setrw /dev/block/mapper/$i;
+  done
+}
 
-sleep 1
-umount /product > /dev/null 2>&1
-umount /vendor > /dev/null 2>&1
-umount /odm > /dev/null 2>&1
-umount /system_ext > /dev/null 2>&1
-umount /system_root > /dev/null 2>&1
 
 sleep 1
 mkdir /tmp/install
 mkdir /tmp/install/bin
 
-exit 0
+set_read_write_partitions;
+exit 0;
 #
